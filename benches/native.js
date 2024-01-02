029da5e1fde04f0f274b11b140576292083b9684
hm-weigle <maximilian.weigle@hm.edu>
2024-01-02 21:17:03 +0100

    add native

---
+++ benches/native.js
@@ -0,0 +1,48 @@
+function objectPropertyAccess() {
+    let obj = { a: { b: { c: 1 } } };
+
+    for (let i = 0; i < 1000; i++) {
+        let value = obj.a.b.c;
+        obj = { a: { b: { c: value } } };
+    }
+
+    return obj;
+}
+
+function stringManipulation() {
+    let str = '';
+    for (let i = 0; i < 30; i++) {
+        str += 'abcdefghijklmnopqrstuvwxyz';
+        str = str.split('').reverse().join('');
+    }
+    return str;
+}
+
+function math() {
+    let baseNumber = Math.pow(4, 7);
+    let result = 0;
+    for (let i = baseNumber; i >= 0; i--) {		
+        result += Math.atan(i) * Math.tan(i);
+    };
+    return result;
+}
+
+const benchmark = (func, params, iterations) => {
+    
+    let time = [];
+    for (let i = 0; i < iterations; i++) {
+        const t0 = performance.now()    
+        func(params)
+        const t1 = performance.now()
+        time.push(t1 - t0);
+    }
+    time.sort();
+    let median = time[time.length / 2];
+
+    
+    return `${func.name}: ${median}ms`
+}
+
+console.log(benchmark(math, null, 200));
+console.log(benchmark(stringManipulation, null, 400));
+console.log(benchmark(objectPropertyAccess, null, 1300)); // iteration count same as in criterion
